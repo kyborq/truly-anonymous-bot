2f9b3e5905c9683ce0ddba19650b37bd48b703fd
root <root@cv4043219.novalocal>
2024-09-21 17:39:46 +0300

    Fixed message sending errors

--- handlers/message_handler.py
+++ handlers/message_handler.py
@@ -3,22 +3,19 @@ from telegram.ext import ContextTypes, filters
 from config import bot_config
 from utils.check_admin import is_admin, get_admin_id
 
-latest_messages = []
+last_messages = []
 
 async def text_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
   admin_id = get_admin_id()
 
   user = update.message.from_user
   text = update.message.text
-  
+
   if not is_admin(user.id):
-    latest_messages.append(text)
-    last_index = len(latest_messages) - 1
+    last_messages.append(text)
     await update.message.reply_text(bot_config["sended"])
-    await context.bot.send_message(chat_id=admin_id, text=latest_messages[last_index])
+    await context.bot.send_message(chat_id=admin_id, text=f"{bot_config['question']}{text}", parse_mode="MarkdownV2")
   else:
-    last_index = len(latest_messages) - 1
-    last_message = latest_messages[last_index]
-    latest_messages.pop(last_index)
-    await update.message.reply_text(f"{bot_config["question"]}{last_message}\n\n{bot_config["answer"]}>{text}", parse_mode="MarkdownV2")
-    
+    last_index = len(last_messages) - 1
+    last_message = last_messages[last_index]
+    await update.message.reply_text(f"{bot_config['question']}{last_message}\n\n{bot_config['answer']}>{text}", parse_mode="MarkdownV2")
--- utils/check_admin.py
+++ utils/check_admin.py
@@ -7,10 +7,9 @@ def get_admin_id():
 
 def is_admin(client_id):
   admin_id = get_admin_id()
-  
+
   if not admin_id:
     print(f"Add ADMIN_CHAT_ID={client_id} to .env file")
     return False
 
-  print(f"{client_id} == {admin_id}")
   return str(client_id) == str(admin_id)
